6a9f89eaaa2f7f758578b629329a5cf6e385a6a2
PawanNahar <pawannahar73@gmail.com>
2023-06-25 21:40:23 +0530

    UpdatedBatchfile

--- Selenium_Pytest_HybridFramework_POM__Project/BatchFile/All_Libraries.bat
+++ Selenium_Pytest_HybridFramework_POM__Project/BatchFile/All_Libraries.bat
@@ -1,21 +1,9 @@
-import allure
-from allure_commons.types import AttachmentType
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.select import Select
-from selenium.webdriver import ActionChains
-import pytest
-from selenium.webdriver.common.by import By
-from ddt import ddt, data, file_data, unpack
-import unittest
-import allure
-from allure_commons.types import AttachmentType
-import pytest
-import requests
-import json
-import jsonpath
-import logging
-import inspect
-from openpyxl import load_workbook
-import openpyxl
-import csv
+pip install selenium
+pip install ddt
+pip install allure-pytest
+pip install pytest
+pip install requests
+pip install jsonpath
+pip install openpyxl
+pip install pytest-xdist
+pip install pytest-html
